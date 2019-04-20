b4199a2b9f5d7a41488dff2a27d17faaa9106d3e
Max Ryndin <rgmasim@gmail.com>
2019-04-20 13:16:45 +0300

    Show wifi data.

--- app/src/main/java/com/example/myfirstapp/DisplayWifiDataActivity.kt
+++ app/src/main/java/com/example/myfirstapp/DisplayWifiDataActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myfirstapp
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+
 
 class DisplayWifiDataActivity : AppCompatActivity() {
 
@@ -10,8 +14,28 @@ class DisplayWifiDataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_wifi_data)
 
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = "WiFi will be here "
+        val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        val success = wifiManager.startScan()
+        if (!success) {
+            scanFailure()
         }
+
+        scanSuccess(wifiManager)
+    }
+
+    private fun scanSuccess(wifiManager: android.net.wifi.WifiManager) {
+        val results = wifiManager.scanResults
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            var message = ""
+            for (result in results) {
+                message += result.SSID + ",\t\t" + result.BSSID + ",\t\t" + result.level + "\n"
+            }
+            text = message
+        }
+    }
+
+    private fun scanFailure() {
+        Log.e("scanFailure", "")
     }
 }
